a3c4391c66284db53cddc5a1d645627c83f88fed
Lumir Balhar <lbalhar@redhat.com>
2026-03-31 11:01:36 +0200

    Support unittest _SubTest XML output in Python 3.15+
    Python 3.15 generates unittest.case._SubTest objects for subtest failures,
    which produce an additional _SubTest.xml report file containing valuable
    traceback information. Update test expectations to account for this extra
    file (107 files instead of 106) in Python 3.15+.

--- src/zope/testrunner/tests/test_xml_output.py
+++ src/zope/testrunner/tests/test_xml_output.py
@@ -130,7 +130,10 @@ class TextXMLOutputWithErrors(Base):
         self._run_tests()
 
         self.assertTrue(self.reports_folder.exists())
-        self.assertEqual(len([x for x in self.reports_folder.iterdir()]), 106)
+        # Python 3.15+ includes an additional unittest.case._SubTest.xml file
+        # containing subtest information
+        expected_count = 107 if sys.version_info >= (3, 15) else 106
+        self.assertEqual(len([x for x in self.reports_folder.iterdir()]), expected_count)
 
     def test_xml_report_with_errors_details(self):
         sys.argv = 'test --tests-pattern ^sampletests(f|_e|_f)?$ '.split()
